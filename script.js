7b98ae6a11c1d083044be552701b8eec003d7807
Kalpna <132220523+kalpna141@users.noreply.github.com>
2023-06-27 17:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,54 +1,25 @@
-//your code here
-// let arr=['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-// let updateList=arr.map*((element)=>
-// 	{
-// 		let words=element.split(" ");
-// 		let updateString=words.reduce((prev, current, index)=>{
-// 			if(current!='The' && current!="an" && current!="a"){
-// 				return prev + " " + current;
-// 			}
-// 			return prev;
-// 		}, "")
-// return updateString;
-// });
-// let mp={};
-// updateList.forEach( (element, index) =>{
-// 	mp[element]= arr[index];
-// });
-// updateList.sort();
-// let finalAns=updateList.map((element)=>{
-// 	return mp[element];
-// })
-// const ul=document.getElementById("band")
-// finalAns.forEach((item)=>
-// 	{
-// 		const li=document.createElement("li");
-// 		li.innerText=item;
-// 		ul.append(li);
-// 	})
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+bandNames.sort((a,b)=>(strip(a)>strip(b))?1:-1)
+const ref= document.getElementByTagName("ul")[0]
+for(let i=0; i<bandNames.length; i++)
+	{
+		const li=document.createElement("li")
+		li.innerText=bandNames[i]
+		ref.append(li)
+	}
 
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a, b) => {
-    const articles = /^(The|A|An)\s+/i; 
+function strip(word) {
+	let arr=word.split(" ")
+	let s=""
+	for(let i=0; i<arr.length; i++){
+		if(articles.indexOf(arr[i].toUpperCase())=== -1)
 
-    const spotA = a.replace(articles, '');
-    const spotB = b.replace(articles, '');
-  
-    return spotA.localeCompare(spotB);
-  });
-console.log(touristSpots);
-  
- const ulElement = document.getElementById('band');
-
-
-ulElement.innerHTML = '';
-
-
-touristSpots.forEach((spot) => {
-  const liElement = document.createElement('li');
-  liElement.textContent = spot;
-  ulElement.appendChild(liElement);
-});
-						   
+		{
+			s=s+arr[i];
+		}
+	
+}
+ return s.trim()
 				  
-			
+}			
